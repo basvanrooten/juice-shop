6bb5c50e6496a7f305494342a142d07357542600
Tom Smits <tmsmits@gmail.com>
2020-02-20 13:52:14 +0100

    Fixed product Model

--- models/product.js
+++ models/product.js
@@ -10,13 +10,7 @@ const challenges = require('../data/datacache').challenges
 module.exports = (sequelize, { STRING, DECIMAL }) => {
   const Product = sequelize.define('Product', {
     name: STRING,
-    description: {
-      type: STRING,
-      set (description) {
-        utils.solveIf(challenges.restfulXssChallenge, () => { return utils.contains(description, '<iframe src="javascript:alert(`xss`)">') })
-        this.setDataValue('description', description)
-      }
-    },
+    description: STRING,
     price: DECIMAL,
     deluxePrice: DECIMAL,
     image: STRING
